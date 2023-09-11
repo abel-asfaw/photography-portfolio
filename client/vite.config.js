3757aa40733758e1c48602fe896b3fa2412aef45
Abel <dev@abelasfaw.com>
2023-09-11 08:49:20 -0700

    feat: Implement Nginx reverse proxy for frontend and API routing
    - Set up Nginx to listen on port 80 for incoming HTTP connections.
    - Configured maximum client request body size to 20M for handling large file uploads.
    - Forwarded requests to root path to frontend service on port 3000 with WebSocket support.
    - Directed requests with /api/ prefix to API service on port 4000, preserving original client details.
    - Added internal directive for /404.html to prevent direct client access.
    - Thanks to Manny for the instructions https://stackoverflow.com/questions/62562017/docker-compose-with-nginx-reverse-a-website-and-a-restful-api

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [react()],
     server: {
         host: '0.0.0.0',
         port: 3000,
     },
-    plugins: [react()],
-    envDir: '../',
+    hmr: {
+        clientPort: 3000,
+    },
 });
--- database/main.py
+++ database/main.py
@@ -15,11 +15,11 @@ class PhotoModel(BaseModel):
     photo_url: str
 
 
-app = FastAPI(debug=True)
+app = FastAPI(root_path="/api", debug=True)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:4002"],
+    allow_origins=[],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -117,4 +117,4 @@ async def get_photos():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=4001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
