ecfe79377319cfe5bf757c2ca6577325c2368dce
Abel <dev@abelasfaw.com>
2023-09-14 11:58:38 -0700

    feat: Overlay delete button on photo

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -8,14 +8,17 @@ export default function Photo({ photo, ...rest }) {
     };
 
     return (
-        <div>
-            <img
-                src={photo.photo_url}
-                className="h-auto w-96 transform-gpu rounded-2xl duration-700 hover:scale-110"
-                {...rest}
-            />
-            <div className="actions">
-                <button className="bg-slate-300" onClick={handleDelete}>
+        <div className="group relative w-96 h-auto">
+            <div className="transform-gpu duration-700 hover:scale-110 relative rounded-2xl overflow-hidden">
+                <img
+                    src={photo.photo_url}
+                    className="h-auto w-full"
+                    {...rest}
+                />
+                <button
+                    onClick={handleDelete}
+                    className="hidden group-hover:block bg-slate-300 absolute top-2 right-2"
+                >
                     Delete
                 </button>
             </div>
