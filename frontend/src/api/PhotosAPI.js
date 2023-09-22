327a16fa2dba2c5aba88d692f1c92c7ee67da7bb
Abel <dev@abelasfaw.com>
2023-09-22 14:17:54 -0700

    clean: Remove template literals where not needed

--- frontend/src/api/PhotosAPI.js
+++ frontend/src/api/PhotosAPI.js
@@ -5,13 +5,13 @@ const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/photos`;
 export const uploadPhoto = async file => {
     const formData = new FormData();
     formData.append('file', file, file.name);
-    const response = await axios.post(API_BASE_URL, formData );
+    const response = await axios.post(API_BASE_URL, formData);
 
     return response.data;
 };
 
 export const fetchPhotos = async () => {
-    const response = await axios.get(`${API_BASE_URL}`);
+    const response = await axios.get(API_BASE_URL);
 
     return response.data;
 };
