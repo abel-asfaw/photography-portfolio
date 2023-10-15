28312d1601864a0e128ef0f6c6e90f32e2047989
Abel <dev@abelasfaw.com>
2023-10-15 10:49:27 -0700

    style: Add hover animation to FileInput

--- frontend/src/components/FileInput.jsx
+++ frontend/src/components/FileInput.jsx
@@ -7,9 +7,9 @@ export default function FileInput({
     ...rest
 }) {
     const classes = classNames(
-        'h-10 file:h-10 rounded-md bg-zinc-800 text-sm text-gray-400 file:mr-2 file:rounded-md',
-        'file:rounded-r-none file:border-0 file:bg-sky-500 file:px-4 file:py-2',
-        'file:text-sm file:font-semibold file:text-white hover:file:bg-sky-600',
+        'h-10 rounded-md bg-zinc-800 text-sm text-gray-400 file:mr-2 file:h-10 file:cursor-pointer',
+        'file:rounded-md file:rounded-r-none file:border-0 file:bg-sky-500 file:px-4 file:py-2',
+        'file:text-sm file:font-semibold file:text-white file:duration-200 hover:file:bg-sky-600',
         className,
     );
 
