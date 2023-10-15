234a2a2a2828e684d091777169bdb2b42d53b0dc
Abel <dev@abelasfaw.com>
2023-10-14 23:59:53 -0700

    style: Set Button and FileInput height for consistency

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -13,8 +13,8 @@ export default function Button({
     const classes = classNames(
         'flex text-sm items-center justify-center duration-300',
         {
-            'rounded-full p-3': circular,
-            'rounded-md px-4 py-2': !circular,
+            'h-10 w-10 rounded-full p-3': circular,
+            'h-10 rounded-md px-4 py-2': !circular,
             'bg-gray-50 hover:bg-gray-300': primary,
             'bg-green-600 hover:bg-green-700': success,
             'bg-red-600 hover:bg-red-700': danger,
--- frontend/src/components/FileInput.jsx
+++ frontend/src/components/FileInput.jsx
@@ -1,8 +1,13 @@
 import classNames from 'classnames';
 
-export default function FileInput({ fileInputRef, onInputChange, className, ...rest }) {
+export default function FileInput({
+    fileInputRef,
+    onInputChange,
+    className,
+    ...rest
+}) {
     const classes = classNames(
-        'rounded-md bg-zinc-800 text-sm text-gray-400 file:mr-2 file:rounded-md',
+        'h-10 file:h-10 rounded-md bg-zinc-800 text-sm text-gray-400 file:mr-2 file:rounded-md',
         'file:rounded-r-none file:border-0 file:bg-sky-500 file:px-4 file:py-2',
         'file:text-sm file:font-semibold file:text-white hover:file:bg-sky-600',
         className,
