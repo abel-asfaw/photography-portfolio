b1c786c2718d73f8b29ffaa0499a2fc5ce7dd760
Abel <dev@abelasfaw.com>
2025-03-17 15:44:53 -0400

    chore: Update config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +8,8 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 3000,
-        hmr: {
-            clientPort: 3000,
-        },
+    },
+    resolve: {
+        alias: [{ find: '@', replacement: resolve(__dirname, './') }],
     },
 });
