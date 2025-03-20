f807ea114b8ea69521141b3005dd6d7bcf8476ef
Abel <dev@abelasfaw.com>
2025-03-20 18:34:04 -0500

    fix: This host ("client") is not allowed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 3000,
+        allowedHosts: ['client'],
     },
     resolve: {
         alias: [{ find: '@', replacement: resolve(__dirname, './') }],
