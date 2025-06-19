9d8339135157b1d0b80fa4a6b7d61b11f7a427cc
Abel <dev@abelasfaw.com>
2025-06-19 04:15:41 -0400

    chore: Update tailwind stuff

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,18 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-
-import postcss from './postcss.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-        allowedHosts: ['client'],
-    },
-    resolve: {
-        alias: [{ find: '@', replacement: resolve(__dirname, './') }],
-    },
-    css: {
-        postcss,
-    },
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['client'],
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './') }],
+  },
 });
