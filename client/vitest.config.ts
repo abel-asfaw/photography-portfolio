014083d8eda2a10d292bc9e9c168862eba2e359e
Abel <dev@abelasfaw.com>
2026-02-09 18:24:03 -0500

    chore: Add testing setup with Vitest and React Testing Library

---
+++ client/vitest.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './') }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
+});
